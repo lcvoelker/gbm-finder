de5e181a7ba0b49e205f08fcae7c83be11a744cb
lcvoelker <123799746+lcvoelker@users.noreply.github.com>
2023-01-28 02:37:55 -0500

    updated app.js
    imported useEffect, useState, 
    part of display that lists array of users on page (not necessary but could be part of tracking accounts made on the site)

--- gbm-finder-frontend/src/App.js
+++ gbm-finder-frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useEffect, useState } from 'react'
 // components
 import PostRow from './components/postRow'
 
@@ -7,10 +8,31 @@ function App() {
         { name: "Today" },
         { name: "This week" },
     ]
+    
+    const [backendData, setBackendData] = useState([{ }])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+    
   return (
     <div className="App">
         <PostRow metadata={rows[0]}/>
         <PostRow metadata={rows[1]}/>
+
+        {(typeof backendData.users === 'undefined') ? (
+        <p>Loading!</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
     </div>
   );
 }
